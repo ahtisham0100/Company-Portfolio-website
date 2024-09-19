3da3a8a4e9ab7573986296a4339f4f1fddc02841
Muhammad Ahtisham Arif <ahtisham0100@gmail.com>
2024-09-19 23:12:47 +0500

    starting dsa

---
+++ CodeSpace/DSA/main.java
@@ -0,0 +1,13 @@
+package CodeSpace.DSA;
+
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+
+        System.out.println("hello");
+Scanner in = new Scanner(System.in);
+      String name =  in.next();
+        System.out.println(name);
+    }
+}
