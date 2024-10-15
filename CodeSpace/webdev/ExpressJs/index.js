e0fcaf2c01bb3d6f4a251f74314098b6cddd4fa7
Muhammad Ahtisham Arif <ahtisham0100@gmail.com>
2024-10-15 16:06:03 +0500

    psh

--- CodeSpace/webdev/ExpressJs/index.js
+++ CodeSpace/webdev/ExpressJs/index.js
@@ -1,10 +1,12 @@
 import express from "express"; 
 const app =  express();
-const port = 3000;
+const port = 3200;
 
 app.get('/' ,(req , res )=>{
    res.send("Hello world !");
 });
+
 app.listen(port, ()=>{ 
-    console.log("server is running on the port 3000");
+    console.log(`server is running on the port ${ port }`);
+
 }) ;
