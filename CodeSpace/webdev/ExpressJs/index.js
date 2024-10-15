6ad6e8fb1752eee5d41de90e00797cdda5b8ec09
Muhammad Ahtisham Arif <ahtisham0100@gmail.com>
2024-10-15 16:09:47 +0500

    express

--- CodeSpace/webdev/ExpressJs/index.js
+++ CodeSpace/webdev/ExpressJs/index.js
@@ -3,8 +3,8 @@ const app =  express();
 const port = 3200;
 
 app.get('/' ,(req , res )=>{
-   res.send("Hello world !");
-});
+   res.send(  `index.html`);
+    });
 
 app.listen(port, ()=>{ 
     console.log(`server is running on the port ${ port }`);
